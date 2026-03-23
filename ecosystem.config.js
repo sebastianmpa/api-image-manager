c98deada3633c93c29fd21ae9a305d92701e70d7
abel-pomares-prontomowers <abelp.prontomowers@gmail.com>
2026-03-23 10:57:41 -0400

    Ajustes despliegue en PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,17 +2,13 @@ module.exports = {
   apps: [
     {
       name: "PRONTO-API-IMAGE-MANAGER",
-      script: "uvicorn",
-      args: "app.main:app --reload --host 0.0.0.0 --port 3600",
-      interpreter: "C:\\Users\\sebastian\\Desktop\\optimized_image\\.venv\\Scripts\\python",
-      watch: true,
-      ignore_watch: ["node_modules", "__pycache__", ".venv", "*.log"],
-      env: {
-        NODE_ENV: "development",
-      },
-      error_file: "./logs/error.log",
-      out_file: "./logs/out.log",
-      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
-    },
-  ],
+      script: "./venv/bin/uvicorn",
+      args: "app.main:app --host 0.0.0.0 --port 3600",
+      cwd: "/home/administrator/apps/services/api-image-manager",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "300M"
+    }
+  ]
 };
